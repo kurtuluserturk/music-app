9b236f6a6e12d00bb6ea0297a10b3a3f15170cfe
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-07-03 14:50:03 +0300

    page was styled

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
         },
         {
           id: 3,
-          title: "short song",
+          title: "Short Song",
           artist: "TEST",
           src: require("./assets/short-song.mp3"),
         },
@@ -132,5 +132,98 @@ header {
   color: #ffa31a;
 }
 
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
 
+.song-title {
+  color: #53565A;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: #ffa31a;
+  background-color: #1b1b1b;
+}
+
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #FFF;
+  background-color: #808080;
+  transition: all 0.4s;
+}
+
+.next:hover, .prev:hover {
+  color: #ffa31a;
+  background-color: #1b1b1b;
+}
+
+.playlist {
+  padding: 0px 30px;
+}
+
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.playlist .song:hover {
+  color: #ffa31a;
+}
+
+.playlist .song.playing {
+  color: #FFF;
+  background-image: linear-gradient(to right, #ffa31a, #FF5858);
+}
 </style>
